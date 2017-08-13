0a66934c64bd52e6030a6638209646a0c3fd5b8b
Glenlivet Zhang <glenlivet.zhang@pwc.com>
2017-08-13 23:09:08 +0800

    unused classes removed.

--- src/main/java/org/glenlivet/sakura/mongo/repository/ProductSkuMongoRepository.java
+++
@@ -1,7 +0,0 @@
-package org.glenlivet.sakura.mongo.repository;
-
-import org.glenlivet.sakura.mongo.domain.ProductSku;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface ProductSkuMongoRepository extends MongoRepository<ProductSku, Long> {
-}
